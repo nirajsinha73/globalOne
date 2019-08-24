4d0ba5c95d9bba08378e61c2fc40343803ff8834
aravindanath <aravindanath86@gmail.com>
2019-08-24 09:46:46 +0530

    updated @suit

--- src/test/java/Day1/LaunchBrowser.java
+++ src/test/java/Day1/LaunchBrowser.java
@@ -1,15 +1,20 @@
 package Day1;
 
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 public class LaunchBrowser {
 	
+	@BeforeSuite
+	public void beforeSuite() {
+		System.err.println(" ========== Connect to db ========== ");
+	}
 	
 
-	@BeforeClass
+	@AfterSuite
 	public void OpenBrowser() {
-		System.err.println("Open Browser");
+		System.err.println(" ========== close db ========== ");
 	}
 	
 
--- src/test/java/Day1/TC001.java
+++ src/test/java/Day1/TC001.java
@@ -1,55 +1,50 @@
 package Day1;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
-public class TC001  extends LaunchBrowser{
+public class TC001 extends LaunchBrowser {
 
-	
-	
+	@BeforeMethod
+	public void beforeMethod() {
+		System.err.println(" ====== Before method =====");
+	}
 
-	
-	
+	@AfterMethod
+	public void afterMethod() {
+		System.err.println(" ====== After method =====");
+	}
+
 	@Test
 	public void url() {
 		System.out.println("open url");
 	}
-	
+
 	@Test
 	public void search() {
 		System.out.println("search");
 	}
-	
-	
+
 	@Test
 	public void login() {
 		System.out.println("login");
 	}
-	
-	
+
 	@Test
 	public void addCart() {
 		System.out.println("add cart");
-		
+
 		Assert.fail("Fail...");
 	}
-	 
-	
-	
+
 	@Test
 	public void addAdd() {
 		System.out.println("add Add");
 	}
-	
-	
+
 	@Test
 	public void cod() {
 		System.out.println("cod");
 	}
- 
-	
-	
+
 }
